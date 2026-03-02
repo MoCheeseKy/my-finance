30e090df5a3250b5595858a6b6554dcb87f0ce93
Your NameMoCheeseKy <rifkyking04@gmail.com>
2026-03-03 00:51:09 +0700

    ,

--- src/lib/storage.js
+++ src/lib/storage.js
@@ -9,20 +9,4 @@ export const db = localforage.createInstance({
 // Fungsi helper buat inisialisasi dummy data kalau app baru pertama kali dibuka
 export const initDummyData = async () => {
   const isInitialized = await db.getItem('is_initialized');
-  if (!isInitialized) {
-    await db.setItem('balance', 4550000);
-    await db.setItem('income', 2100000);
-    await db.setItem('expense', 850000);
-    await db.setItem('savings', {
-      name: 'Macbook Air M3',
-      target: 15000000,
-      current: 4500000,
-    });
-    await db.setItem('upcoming_bill', {
-      name: 'Spotify Premium',
-      amount: 54900,
-      daysLeft: 2,
-    });
-    await db.setItem('is_initialized', true);
-  }
 };
